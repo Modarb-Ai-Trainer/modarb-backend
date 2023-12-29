8a36cc543c649d90c420a2598a85acd5cb589fa7
mowafy001 <wafy123445@gmail.com>
2023-12-29 05:40:40 +0200

    refactor: more small refactors

--- src/index.route.ts
+++ src/index.route.ts
@@ -8,16 +8,11 @@ const mainRouter = express.Router();
 mainRouter.use("/user", userRoutes);
 mainRouter.use("/admin", adminRoutes);
 
-mainRouter.get("/", (_req: any, res: any) => {
-  res
-    .status(200)
-    .json({ success: true, message: "Welcome Message", code: 200 });
-});
-
 mainRouter.get("/health", (_req: any, res: any) => {
   res.status(200).json({ success: true, message: "Server is up!", code: 200 });
 });
 
+// catch non existing routes
 mainRouter.all("*", (_req: any, res: any) => {
   res.status(404).json({ success: false, message: "Invalid URL!", code: 404 });
 });
