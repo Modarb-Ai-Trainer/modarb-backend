7933ed3804078ddd968abe627521a4925ed4820f
mowafy001 <wafy123445@gmail.com>
2023-12-29 05:09:50 +0200

    refactor: index and app

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,27 @@
-import { app } from "./configs/app";
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import { routes } from "./index.route";
+import { connection as databaseConnection } from "./configs/database";
 
-app.listen(process.env.PORT || 4000, () => {
-  console.log(`Server is up and runing on port ${process.env.PORT}!`);
-});
+const main = async () => {
+  // set up database connection
+  databaseConnection();
+
+  // set up express server
+  const app = express();
+  app.use(cors());
+  app.use(express.json());
+  app.use(routes);
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+
+  // start server
+  app.listen(process.env.PORT || 4000, () => {
+    console.log(`Server is up and running on port ${process.env.PORT}!`);
+  });
+};
+
+main();
