4feafca4b551834cc240d278fbecfaafa158fcdc
mowafy001 <wafy123445@gmail.com>
2023-12-29 05:37:48 +0200

    refactor: small refactors

--- src/index.route.ts
+++ src/index.route.ts
@@ -1,30 +1,25 @@
 import express from "express";
 
-const app = express.Router();
-
 import { userRoutes } from "./modules/user/index.route";
 import { adminRoutes } from "./modules/console/index.route";
 
-// set prefix for all routes
-const v1Router = express.Router();
+const mainRouter = express.Router();
 
-v1Router.use("/user", userRoutes);
-v1Router.use("/admin", adminRoutes);
+mainRouter.use("/user", userRoutes);
+mainRouter.use("/admin", adminRoutes);
 
-v1Router.get("/", (_req: any, res: any) => {
+mainRouter.get("/", (_req: any, res: any) => {
   res
     .status(200)
     .json({ success: true, message: "Welcome Message", code: 200 });
 });
 
-v1Router.get("/health", (_req: any, res: any) => {
+mainRouter.get("/health", (_req: any, res: any) => {
   res.status(200).json({ success: true, message: "Server is up!", code: 200 });
 });
 
-v1Router.all("*", (_req: any, res: any) => {
+mainRouter.all("*", (_req: any, res: any) => {
   res.status(404).json({ success: false, message: "Invalid URL!", code: 404 });
 });
 
-app.use("/api/v1", v1Router);
-
-export { app as routes };
+export { mainRouter };
