1be662ea37dcb9268a8276d244bed71e1cc89493
mowafy001 <wafy123445@gmail.com>
2023-12-29 05:09:50 +0200

    refactor: index and app

--- src/index.route.ts
+++ src/index.route.ts
@@ -1,38 +1,30 @@
-import express from 'express';
+import express from "express";
+
 const app = express.Router();
 
-import { userRoutes } from './modules/user/index.route';
-import { adminRoutes } from './modules/console/index.route';
+import { userRoutes } from "./modules/user/index.route";
+import { adminRoutes } from "./modules/console/index.route";
 
-app.use("/api/v1/user", userRoutes);
-app.use("/api/v1/admin", adminRoutes);
+// set prefix for all routes
+const v1Router = express.Router();
 
+v1Router.use("/user", userRoutes);
+v1Router.use("/admin", adminRoutes);
 
-app.get("/", (req: any, res: any) => {
-    res.status(200).json({ success: true, message: 'Welcome Message', code: 200 })
-})
+v1Router.get("/", (_req: any, res: any) => {
+  res
+    .status(200)
+    .json({ success: true, message: "Welcome Message", code: 200 });
+});
 
-app.get("*", (req: any, res: any) => {
-    res.status(404).json({ success: false, message: "Invalid URL!", code: 404 })
-})
+v1Router.get("/health", (_req: any, res: any) => {
+  res.status(200).json({ success: true, message: "Server is up!", code: 200 });
+});
 
-app.post("*", (req: any, res: any) => {
-    res.status(404).json({ success: false, message: "Invalid URL!", code: 404 })
-})
+v1Router.all("*", (_req: any, res: any) => {
+  res.status(404).json({ success: false, message: "Invalid URL!", code: 404 });
+});
 
-app.put("*", (req: any, res: any) => {
-    res.status(404).json({ success: false, message: "Invalid URL!", code: 404 })
-})
-
-app.delete("*", (req: any, res: any) => {
-    res.status(404).json({ success: false, message: "Invalid URL!", code: 404 })
-})
-
-
-app.patch("*", (req: any, res: any) => {
-    res.status(404).json({ success: false, message: "Invalid URL!", code: 404 })
-})
-
-
+app.use("/api/v1", v1Router);
 
 export { app as routes };
