74e470afc4e92b54893e2965fa1b3bc1d12ae057
mowafy001 <wafy123445@gmail.com>
2023-12-29 05:31:00 +0200

    refactor: base and config

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,27 @@
-import dotenv from "dotenv";
-dotenv.config();
 import express from "express";
-import bodyParser from "body-parser";
 import cors from "cors";
 import { routes } from "./index.route";
-import { connection as databaseConnection } from "./configs/database";
+import { connectDatabase } from "./configs/database";
+import { config } from "./configs/config";
 
 const main = async () => {
   // set up database connection
-  databaseConnection();
+  connectDatabase();
 
   // set up express server
   const app = express();
+
+  // set up middlewares
   app.use(cors());
+  app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
+
+  // set up routes
   app.use(routes);
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: false }));
 
   // start server
-  app.listen(process.env.PORT || 4000, () => {
-    console.log(`Server is up and running on port ${process.env.PORT}!`);
+  app.listen(config.port, () => {
+    console.log(`Server is up and running on port ${config.port}!`);
   });
 };
 
