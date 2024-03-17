87364ee50adf7e098965c0ab3ee61aef77a5f1a3
mowafy001 <wafy123445@gmail.com>
2024-03-17 11:30:54 +0200

    chore: rename userModel to User

--- src/modules/users/modules/auth/services/users-auth.service.ts
+++ src/modules/users/modules/auth/services/users-auth.service.ts
@@ -2,11 +2,11 @@ import bcrypt from "bcrypt";
 import { ILogin } from "../validation/login.validation";
 import { HttpError } from "@lib/error-handling/http-error";
 import { JwtHelper } from "@helpers/jwt.helper";
-import { userModel } from "@common/models/user.model";
+import { User } from "@common/models/user.model";
 import { IUserRegister } from "@common/validations/user-register.validation";
 import { CrudService } from "@lib/services/crud.service";
 
-export class UsersAuthService extends CrudService(userModel) {
+export class UsersAuthService extends CrudService(User) {
   async register(createParams: IUserRegister) {
     return this.create(createParams);
   }
