b931eee5e88b5c5953713b90b6b8445cfee4dca1
youssefkatry907 <youssefkatry907@gmail.com>
2024-03-25 21:08:58 +0200

    Workout module

--- src/common/models/workout.model.ts
+++ src/common/models/workout.model.ts
@@ -3,10 +3,42 @@ const { Schema } = mongoose;
 
 export interface IWorkout {
     name: string;
+    type: string;
+    created_by: mongoose.Types.ObjectId;
+    templateWeeks: [
+        {
+            days: [
+                {
+                    day: number,
+                    exercises: [
+                        {
+                            exercise: mongoose.Types.ObjectId,
+                        },
+                    ],
+                },
+            ],
+        },
+    ]
 }
 
 const workoutSchema = new Schema({
     name: { type: String, required: true, unique: true, dropDups: true },
+    type: { type: String, required: true },
+    created_by: { type: mongoose.Types.ObjectId, ref: "users" },
+    templateWeeks: [
+        {
+            days: [
+                {
+                    day: Number,
+                    exercises: [
+                        {
+                            exercise: { type: mongoose.Types.ObjectId, ref: "exercises" },
+                        },
+                    ],
+                },
+            ],
+        },
+    ]
 });
 
 
---
+++ src/modules/console/modules/admins/validations/create-workout.validation.ts
@@ -0,0 +1,33 @@
+import * as joi from "joi";
+import { createSchema } from "@helpers/create-schema";
+
+export interface ICreateWorkout {
+  name: string;
+  type: string;
+  created_by: string;
+  templateWeeks: string[];
+};
+
+
+export const createWorkoutSchema = createSchema<ICreateWorkout>({
+    name: joi.string().empty().required().messages({
+        "string.base": "please enter a valid name",
+        "any.required": "name is required",
+        "string.empty": "name can not be empty",
+    }),
+    type: joi.string().empty().required().messages({
+        "string.base": "please enter a valid type",
+        "any.required": "type is required",
+        "string.empty": "type can not be empty",
+    }),
+    created_by: joi.string().empty().required().messages({
+        "string.base": "please enter a valid created_by",
+        "any.required": "created_by is required",
+        "string.empty": "created_by can not be empty",
+    }),
+    templateWeeks: joi.array().empty().required().messages({
+        "string.base": "please enter a valid templateWeeks",
+        "any.required": "templateWeeks is required",
+        "string.empty": "templateWeeks can not be empty",
+    }),
+});
