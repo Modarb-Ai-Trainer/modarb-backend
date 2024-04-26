267af53fb15e2b8c68774caba19ad0fc44e88ab2
mowafy001 <wafy123445@gmail.com>
2024-04-27 00:11:14 +0300

    feat: seeder

--- src/configs/database.ts
+++ src/configs/database.ts
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 import { config } from "./config";
 
 export const connectDatabase = async () => {
-  return mongoose.connect(config.db.uri).then(() => {
+  return mongoose.connect(config.db.uri).then((o) => {
     console.log(`Connected to MongoDB database successfully!`);
+    return o;
   });
 };
