d1cdbd5399da134b338668b533dca8c9cc08f7eb
mowafy001 <wafy123445@gmail.com>
2024-05-14 03:25:28 +0300

    update: workout progress

--- src/modules/users/modules/user-registered-workouts/controllers/user-registered-workouts.controller.ts
+++ src/modules/users/modules/user-registered-workouts/controllers/user-registered-workouts.controller.ts
@@ -2,7 +2,7 @@ import { UserRegisteredWorkoutsService } from "../services/user-registered-worko
 import { Request, Response } from "express";
 import { JsonResponse } from "@lib/responses/json-response";
 import { parsePaginationQuery } from "@helpers/pagination";
-import { paramsValidator, bodyValidator } from "@helpers/validation.helper";
+import { bodyValidator } from "@helpers/validation.helper";
 import { asyncHandler } from "@helpers/async-handler";
 import { BaseController } from "@lib/controllers/controller.base";
 import { Controller } from "@lib/decorators/controller.decorator";
@@ -14,12 +14,14 @@ import { UsersGuardMiddleware } from "modules/users/common/guards/users.guard";
 import { createUserRegisteredWorkoutsSchema } from "../validations/create-user-registered-workouts.validation";
 import {
   SwaggerGet,
+  SwaggerPatch,
   SwaggerPost,
 } from "@lib/decorators/swagger-routes.decorator";
 import { SwaggerSummary } from "@lib/decorators/swagger-summary.decorator";
 import { SwaggerDescription } from "@lib/decorators/swagger-description.decorator";
 import { SwaggerResponse } from "@lib/decorators/swagger-response.decorator";
-import { SwaggerRequest } from "@lib/decorators/swagger-request.decorator"; 4
+import { SwaggerRequest } from "@lib/decorators/swagger-request.decorator";import { updateUserRegisteredWorkoutsSchema } from "../validations/update-user-registered-workouts.validation";
+ 4
 
 
 interface userRequest extends Request {
@@ -33,40 +35,45 @@ export class userRegisteredWorkoutsController extends BaseController {
 
   setRoutes(): void {
     this.router.get("/:id", asyncHandler(this.get));
-    // this.router.get("/", asyncHandler(this.list));
-    // this.router.post(
-    //   "/",
-    //   bodyValidator(createUserRegisteredWorkoutsSchema),
-    //   asyncHandler(this.create)
-    // );
+    this.router.get("/", asyncHandler(this.list));
+    this.router.post(
+      "/",
+      bodyValidator(createUserRegisteredWorkoutsSchema),
+      asyncHandler(this.create)
+    );
+    this.router.patch(
+      "/:id/progress/:week/:day",
+      bodyValidator(updateUserRegisteredWorkoutsSchema),
+      asyncHandler(this.updateProgress)
+    );
   }
 
-  // @SwaggerGet()
-  // @SwaggerResponse([UserRegisteredWorkoutsPopulateSerialization])
-  // @SwaggerSummary("List my workouts")
-  // @SwaggerDescription("List all user registered workouts (workouts that the user had started)")
-  // list = async (req: userRequest, res: Response) => {
-  //   const paginationQuery = parsePaginationQuery(req.query);
-  //   const { docs, paginationData } =
-  //     await this.userRegisteredWorkoutsService.list(
-  //       { user: req.jwtPayload.id, is_active: true },
-  //       paginationQuery,
-  //       {
-  //         populateArray: [
-  //           { path: "workout" },
-  //           { path: "weeks.days.exercises" },
-  //         ],
-  //       }
-  //     );
+  @SwaggerGet()
+  @SwaggerResponse([UserRegisteredWorkoutsPopulateSerialization])
+  @SwaggerSummary("List my workouts")
+  @SwaggerDescription("List all user registered workouts (workouts that the user had started)")
+  list = async (req: userRequest, res: Response) => {
+    const paginationQuery = parsePaginationQuery(req.query);
+    const { docs, paginationData } =
+      await this.userRegisteredWorkoutsService.list(
+        { user: req.jwtPayload.id, is_active: true },
+        paginationQuery,
+        {
+          populateArray: [
+            { path: "workout" },
+            { path: "weeks.days.exercises" },
+          ],
+        }
+    );
 
-  //   return JsonResponse.success(
-  //     {
-  //       data: serialize(docs, UserRegisteredWorkoutsPopulateSerialization),
-  //       meta: paginationData,
-  //     },
-  //     res
-  //   );
-  // };
+    return JsonResponse.success(
+      {
+        data: serialize(docs, UserRegisteredWorkoutsPopulateSerialization),
+        meta: paginationData,
+      },
+      res
+    );
+  };
 
   @SwaggerGet("/:id")
   @SwaggerResponse(UserRegisteredWorkoutsPopulateSerialization)
@@ -95,19 +102,39 @@ export class userRegisteredWorkoutsController extends BaseController {
   };
 
 
-  // @SwaggerPost()
-  // @SwaggerResponse(UserRegisteredWorkoutsSerialization)
-  // @SwaggerRequest(createUserRegisteredWorkoutsSchema)
-  // @SwaggerSummary("Create workout")
-  // @SwaggerDescription("Create a new workout for the user")
-  // create = async (req: userRequest, res: Response) => {
-  //   const data = await this.userRegisteredWorkoutsService.create(req.body);
-  //   return JsonResponse.success(
-  //     {
-  //       status: 201,
-  //       data: serialize(data.toJSON(), UserRegisteredWorkoutsSerialization),
-  //     },
-  //     res
-  //   );
-  // };
+  @SwaggerPost()
+  @SwaggerResponse(UserRegisteredWorkoutsSerialization)
+  @SwaggerRequest(createUserRegisteredWorkoutsSchema)
+  @SwaggerSummary("Create workout")
+  @SwaggerDescription("Create a new workout for the user")
+  create = async (req: userRequest, res: Response) => {
+    const data = await this.userRegisteredWorkoutsService.createForUser(req.body, req.jwtPayload.id);
+    return JsonResponse.success(
+      {
+        status: 201,
+        data: serialize(data.toJSON(), UserRegisteredWorkoutsSerialization),
+      },
+      res
+    );
+  };
+
+  @SwaggerPatch('/:id/progress/:week/:day')
+  @SwaggerResponse({})
+  @SwaggerRequest(updateUserRegisteredWorkoutsSchema)
+  @SwaggerSummary("Update Workout Progress")
+  @SwaggerDescription("Update the progress of a workout")
+  updateProgress = async (req: userRequest, res: Response) => {
+    const urwId: string = req.params.id;
+    const weekNumber: number = Number(req.params.week);
+    const dayNumber: number = Number(req.params.day);
+    await this.userRegisteredWorkoutsService.updateForUser({
+      urwId, weekNumber, dayNumber
+    }, req.body, req.jwtPayload.id);
+    return JsonResponse.success(
+      {
+        message: "Workout updated successfully",
+      },
+      res
+    );
+  };
 }
