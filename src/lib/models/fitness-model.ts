181016550e0fb367559c6a348d89017bd4587ea2
MoWafy001 <wafy123445@gmail.com>
2024-06-13 07:22:17 +0300

    feat: create flask server

---
+++ src/lib/models/fitness-model.ts
@@ -0,0 +1,6 @@
+import path from "path"
+
+path.join(__dirname, ..."../../resources/models/fitness_model.pkl".split("/"));
+const modelPath = `${__dirname}/../../resources/models/fitness_model.pkl`
+
+export class FitnessModel {}
