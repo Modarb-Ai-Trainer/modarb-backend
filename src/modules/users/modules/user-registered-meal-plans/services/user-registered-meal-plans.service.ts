24962ceaca8a9542a9b5ba36942d08ac4230664f
Hozifa Elgharbawy <Hozifa.elgharbawy12245533@gmail.com>
2024-06-14 15:07:04 +0300

    feat: Add API endpoint to create user meal plan

--- src/modules/users/modules/user-registered-meal-plans/services/user-registered-meal-plans.service.ts
+++ src/modules/users/modules/user-registered-meal-plans/services/user-registered-meal-plans.service.ts
@@ -1,10 +1,35 @@
 import { UserRegisteredMealPlan } from "@common/models/user-registered-meal-plan.model";
 import { CrudService } from "@lib/services/crud.service";
-import { IUpdateUserRegisteredMealPlan } from "../validations/update-user-registered-meal-plan.validation";
+import { MealPlansService } from "../../meal-plans/services/meal-plans.service";
 import { HttpError } from "@lib/error-handling/http-error";
 
 
 export class UserRegisteredMealPlansService extends CrudService(UserRegisteredMealPlan) {
+  private mealPlansService: MealPlansService = new MealPlansService();
+
+  async unregisterCurrentMealPlan(userId: string) {
+    return await this.updateMany({
+      user: userId,
+      isActive: true,
+    }, {
+      isActive: false,
+    });
+  }
+
+  async createForUser(data: any, userId: string) {
+    const mealPlan = await this.mealPlansService.findOneOrFail({
+      _id: data.mealPlan,
+    });
+
+    await this.unregisterCurrentMealPlan(userId);
+    
+    return await this.create({
+      ...data,
+      user: userId,
+      days: mealPlan.days,
+      isActive: true,
+    });
+  }
     async updateForUser(mealPlanProps: {urwId: string; dayNumber: number}, data: any, userId: string) {
         // find workout
         const mealPlan = await this.findOneOrFail({
